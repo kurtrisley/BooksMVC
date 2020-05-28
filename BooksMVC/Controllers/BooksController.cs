a6e96d28cbdd48721a2414120e5dd5c958c84da6
Kurt Risley <35821759+kurtrisley@users.noreply.github.com>
2020-05-28 12:03:27 -0600

    lunch break push GetBooksResponseModel

--- BooksMVC/Controllers/BooksController.cs
+++ BooksMVC/Controllers/BooksController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksMVC.Domain;
+using BooksMVC.Models.Books;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksMVC.Controllers
 {
     public class BooksController : Controller
     {
-        public IActionResult Index()
+        private readonly BooksDataContext _dataContext;
+
+        public BooksController(BooksDataContext dataContext)
         {
-            return View();
+            _dataContext = dataContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //// NO Model. just serializing the domain objects.
+            //var response = await _dataContext.Books.Where(b => b.InInventory).ToListAsync();
+            //return View(response);
+
+            var response = new GetBooksResponseModel
+            {
+                Books = await _dataContext.Books.Where(b => b.InInventory).Select(b => new BooksResponseItemModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author
+                }).ToListAsync(),
+                NumberOfBooksInInventory = await _dataContext.Books.CountAsync(b => b.InInventory),
+                NumberOfBooksNotInInventory = await _dataContext.Books.CountAsync(b => b.InInventory == false)
+            };
+            return View(response);
         }
     }
 }
